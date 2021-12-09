526ccda3206c57f37682312fe4b8c6e8dc0a54fd
oguzzsancaktar <oguztahasancaktar@gmail.com>
2021-12-09 15:27:04 +0300

    part4 OK

--- part4/blog-app/app.js
+++ part4/blog-app/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const blogRouter = require('./controllers/blog');
 const middleware = require('../folder-structure/utils/middleware');
+const userRouter = require('./controllers/user');
 
 
 const app = express();
@@ -11,11 +12,10 @@ app.use(express.json());
 
 app.use(middleware.requestLogger)
 app.use('/api/blogs', blogRouter);
+app.use('/api/users', userRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler);
-
-
 
 
 module.exports = app;
