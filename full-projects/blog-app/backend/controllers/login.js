1c42eb16ed2fb8328f49589bc3092671bbd82239
oguzzsancaktar <oguztahasancaktar@gmail.com>
2021-12-13 20:07:43 +0300

    PArt5 Excercises

--- full-projects/blog-app/backend/controllers/login.js
+++ full-projects/blog-app/backend/controllers/login.js
@@ -1,5 +1,13 @@
+const User = require('../models/user')
+const jwt = require('jsonwebtoken');
+
 const loginRouter = require('express').Router()
 
-
+loginRouter.post('/', async(req, res) => {
+      const body = req.body
+  
+    const user = await User.find()
+    res.send(user)
+})
 
 module.exports = loginRouter
